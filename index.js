df953ba6bf15c7f43e09cebf129b501557d7b96b
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2020-07-03 22:54:06 +0200

    uibuild

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 morgan.token('data-post', (req) => {
   return JSON.stringify(req.dataPost)
 })
+app.use(express.static('build'))
 
 app.use(cors())
 app.use(express.json())
